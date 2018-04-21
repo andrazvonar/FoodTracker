442b309e380b8051e725a287f001401acb1d04c8
Andraž Zvonar <andrazvonar@me.com>
2018-04-21 19:42:04 +0200

    Implement presistance

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import os.log
 
-class Meal {
+class Meal: NSObject, NSCoding {
     
     //MARK: Properties
 
     var name: String
     var photo: UIImage?
     var rating: Int
+    
+    //MARK: Archiving paths
+    
+    static let DocumentDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentDirectory.appendingPathComponent("meals")
+    
+    //MARK: Types
+    
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
+    
+    //MARK: Description
+    override var description: String {
+        get {
+            return "[\(name), \(rating)]"
+        }
+    }
     
     //MARK: Initialization
     init?(name: String, photo: UIImage?, rating: Int) {
@@ -34,5 +55,29 @@ class Meal {
         self.rating = rating
         
     }
+    
+    //MARK: NSCoding
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String
+            else {
+                os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: .debug)
+                return nil
+        }
+        
+        // Because photo is an optional property of Meal, just use conditional cast.
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        let rating = aDecoder.decodeInteger(forKey: PropertyKey.rating)
+        
+        // Must call designated initializer.
+        self.init(name: name, photo: photo, rating: rating)
+    }
+    
 
 }
